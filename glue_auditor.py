4281e1b471ff6d66b6211c0e8f8ee316e9be6e3b
ashubeginner <ashu.beginner@gmail.com>
2020-01-14 12:54:43 +0530

    Update glue_auditor.py

--- glue_auditor.py
+++ glue_auditor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # This will get details of any Glue Dev Endpoint per availability zone and get the Endpoint Alive Duration In Minutes
+#!/usr/bin/env python3
 
 import boto3
 import datetime
@@ -24,12 +25,27 @@ def lambda_handler(event, context):
             for dev_endpoint in dev_endpoints:
                 endpoint_name = dev_endpoint['EndpointName']
                 endpoint_az = dev_endpoint['AvailabilityZone']
-                created_timestamp = dev_endpoint['CreatedTimestamp']
+                created_timestamp = dev_endpoint['CreatedTimestamp'].replace(tzinfo=None)
                 lastmodified_timestamp = dev_endpoint['LastModifiedTimestamp'].replace(tzinfo=None)
                 now_time = datetime.datetime.now()
-                diff = now_time - lastmodified_timestamp
+                diff = now_time - created_timestamp
                 created_minutes = diff.total_seconds()/60
                 print('EndPoint Name : ' + endpoint_name,   'EndPoint AvailabilityZone : ' + endpoint_az,
                         'EndPointCreated : ' + str(created_timestamp),
                         'EndPointLastModifid : ' + str(lastmodified_timestamp),
-                        'Endpoint Alive Duration In Minutes : ' + str(created_minutes), sep='\n')
+                        'Endpoint Life Duration In Minutes : ' + str(created_minutes), sep='\n')
+                if created_minutes > 120 :
+                    msg = 'EndPoint Name : ' + endpoint_name + '\n' + 'Endpoint Alive Duration In Minutes : ' + str(created_minutes)
+                    sub = "Alert - Glue Dev Endpoint Alive for more than 2 Hours"
+                    publish_to_sns(sub, msg)
+                
+
+def publish_to_sns(sub, msg):
+    topic_arn = "arn:aws:sns:us-west-1:338612595803:GlueDevEndpointEmailTopic"
+    sns = boto3.client("sns")
+    
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=msg,
+        Subject=sub
+    )
